a2975d14f49c7c9ad8b8112ee48e46ed6d450973
Hewitt Wang <wangh3@bxscience.edu>
2024-04-05 11:06:41 -0400

    threads working somewhat

--- Multiplayer/ServerSocket.cpp
+++ Multiplayer/ServerSocket.cpp
@@ -1,23 +1,18 @@
 
 #include "ServerSocket.h"
+#include <thread>
 
 #define MSG_WAITALL 0x8 /* do not complete until packet is completely filled */
 #define bzero(p, size) (void) memset((p), 0, (size));
 
 typedef int socklen_t;
-void ServerSocket::HandleIncomingConnection()
-{
-}
 
-ServerSocket::ServerSocket(short PORT)
+ServerSocket::ServerSocket(short PORT) : port{PORT}
 {
-    char buffer[1024];
-    char response[18384];
     struct sockaddr_in serverAddr, clientAddr;
     int i = 0;
     const char* optval = "1";
     int connected = 0;
-    char tempdir[512];
 
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
@@ -50,21 +45,25 @@ ServerSocket::ServerSocket(short PORT)
 
 void ServerSocket::Listen() 
 {
+    printf("Listening on port %d\n", port);
     while (true)
     {
         unsigned int clientSocket;
         sockaddr_in clientAddr;
         socklen_t clientLength;
 
-        char packet[65536];
+        char name[2048];
 
         clientLength = sizeof(clientAddr);
 
-        puts("Awating connection...");
         clientSocket = accept(sock, (struct sockaddr*)&clientAddr, &clientLength);
-        //printf("Established connection with client %s.\n", inet_nto(clientAddr.sin_addr));
-        recv(clientSocket, packet, sizeof(packet), 0);
-        printf("packet recieved: %s\n", packet);
-        HandleIncomingConnection();
+        recv(clientSocket, name, sizeof(name), 0);
+        std::thread new_thread(&HandleIncomingConnection, ClientConnectionInfo{
+                clientSocket, 
+                clientAddr,
+                name
+            }
+        );
+        new_thread.detach();
     }
 }
--- Multiplayer/ServerSocket.h
+++ Multiplayer/ServerSocket.h
@@ -4,13 +4,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
+#include <string>
+
+#define VERBOSE
+
+struct ClientConnectionInfo 
+{
+	unsigned int sock;
+	sockaddr_in clientAddr;
+	std::string display_name;
+};
 
 class ServerSocket
 {
 protected:
 	unsigned int sock;
+	short port;
 
-	void HandleIncomingConnection();
+	static void HandleIncomingConnection(ClientConnectionInfo connection_info) 
+	{
+		printf("Created new thread for client %s\n", connection_info.display_name.c_str());
+		while (connection_info.sock != INVALID_SOCKET) 
+		{
+
+		}
+		printf("lost connection i guess");
+	}
 
 public:
 	ServerSocket(short PORT);
